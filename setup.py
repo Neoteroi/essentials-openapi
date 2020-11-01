612c775bf97fa27754044fd49915c7130ca4b1f1
Roberto Prevato <roberto.prevato@gmail.com>
2020-11-01 11:09:16 +0100

    v0.0.6 :scroll:

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name="essentials-openapi",
-    version="0.0.5",
+    version="0.0.6",
     description="Classes to generate OpenAPI Documentation v3 and v2, in JSON and YAML",
     long_description=readme(),
     long_description_content_type="text/markdown",
